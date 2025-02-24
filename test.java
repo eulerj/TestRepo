e6f6c5cae4af65bb5db7fb80a61c998873459a13
Justin Euler <euler.justin@gmail.com>
2025-02-23 19:04:59 -0600

    Test for a test branch
    Testing for a new branch test with a int println of 3

--- test.java
+++ test.java
@@ -1,8 +1,10 @@
 public class test{
   public static void main(String[] args) {
+    int i = 3;
     System.out.println("Hellow World");
     System.out.println("Test a new print line");
     System.out.println("New line to test if java will compile");
+    System.out.println(i);
   }
     
 }
