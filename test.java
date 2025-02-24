79be91c0c39eb350e70d665bdaf72ae9d7a280b5
Justin Euler <euler.justin@gmail.com>
2025-02-23 18:54:33 -0600

    Added new line to test.java for testing to compile
    Tested java to compile and run to then push to hub

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class test{
   public static void main(String[] args) {
     System.out.println("Hellow World");
     System.out.println("Test a new print line");
+    System.out.println("New line to test if java will compile");
   }
     
 }
